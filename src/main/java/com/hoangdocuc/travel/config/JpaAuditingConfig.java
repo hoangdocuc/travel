c5e25c3ae836fc782041f0c8ede292b271a44494
Hoang Do Cuc <cuchd@vivas.vn>
2020-12-02 13:45:06 +0700

    hihi

---
+++ src/main/java/com/hoangdocuc/travel/config/JpaAuditingConfig.java
@@ -0,0 +1,4 @@
+package com.hoangdocuc.travel.config;
+
+public class JpaAuditingConfig {
+}
--- src/main/java/com/hoangdocuc/travel/entity/BaseEntity.java
+++ src/main/java/com/hoangdocuc/travel/entity/BaseEntity.java
@@ -1,9 +1,7 @@
 package com.hoangdocuc.travel.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
+import java.util.Date;
 
 @MappedSuperclass
 public class BaseEntity {
@@ -13,5 +11,49 @@ public class BaseEntity {
 
     public Long getId() {
         return id;
+    }
+
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "modified_date")
+    private String modifiedDate;
+
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(String modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
     }
 }
